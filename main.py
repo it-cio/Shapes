a0a6bdb334148243281275d9a560fc370c159d27
Divider <it-cio@mail.ru>
2022-02-14 15:58:27 +0300

    added shape class

--- main.py
+++ main.py
@@ -1,9 +1,88 @@
 from shapes import *
+from tkinter import *
+from tkinter import messagebox
 
 window = Tk()
 window.title("Геометрический калькулятор")
 window.configure(bg='grey20')
 window.overrideredirect(True)
+x = 640
+y = 384
+
+
+def make_canvas():
+    window = Tk()
+    window.title("Холст")
+    canvas = Canvas(window, width=1280, height=768, bg='grey20')
+    canvas.pack()
+    return canvas
+
+
+def draw_circle():
+    canvas = make_canvas()
+    canvas.create_oval(220, 140, 420, 340, width=2, fill='grey20', outline='green yellow')
+
+
+def draw_square():
+    canvas = make_canvas()
+    val = int(entry_square.get())
+    x1 = x-val
+    x2 = x+val
+    y1 = y-val
+    y2 = y+val
+    canvas.create_polygon(x1, y1, x2, y1, x2, y2, x1, y2, width=2, fill='grey20', outline='green yellow')
+    square = Square(val)
+    messagebox.showinfo("Расчет", f"Площадь: {square.area()}\nПериметр: {square.perimetr()}")
+
+
+def draw_rectangle():
+    canvas = make_canvas()
+    canvas.create_polygon(220, 200, 420, 200, 420, 280, 220, 280, width=2, fill='grey20', outline='green yellow')
+
+
+def draw_triangle():
+    canvas = make_canvas()
+    canvas.create_polygon(320, 140, 220, 340, 420, 340, width=2, fill='grey20', outline='green yellow')
+
+
+def draw_trapeze():
+    canvas = make_canvas()
+    canvas.create_polygon(270, 200, 370, 200, 420, 280, 220, 280, width=2, fill='grey20', outline='green yellow')
+
+
+def draw_rhombus():
+    canvas = make_canvas()
+    canvas.create_polygon(320, 140, 370, 240, 320, 340, 270, 240, width=2, fill='grey20', outline='green yellow')
+
+
+def draw_sphere():
+    canvas = make_canvas()
+    pass
+
+
+def draw_cube():
+    canvas = make_canvas()
+    pass
+
+
+def draw_parallelepiped():
+    canvas = make_canvas()
+    pass
+
+
+def draw_pyramid():
+    canvas = make_canvas()
+    pass
+
+
+def draw_cylinder():
+    canvas = make_canvas()
+    pass
+
+
+def draw_cone():
+    canvas = make_canvas()
+    pass
 
 
 # Плоские фигуры
@@ -21,8 +100,8 @@ Button(window, text="Квадрат", width=14, height=2, bg='grey30', fg='green
     .grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
 Label(window, text="Сторона квадрата", font=('Arial Bold', 8), bg='grey20', fg='green') \
     .grid(row=2, column=1, padx=5, sticky=W)
-Entry(window, bg='grey30', fg='green yellow')\
-    .grid(row=3, column=1, padx=5, sticky='nsew')
+entry_square = Entry(window, bg='grey30', fg='green yellow')
+entry_square.grid(row=3, column=1, padx=5, sticky='nsew')
 
 Button(window, text="Прямоугольник", width=14, height=2, bg='grey30', fg='green yellow', command=draw_rectangle) \
     .grid(row=1, column=2, padx=5, pady=5, sticky='nsew')
@@ -97,7 +176,7 @@ Button(window, text="Цилиндр", width=14, height=2, bg='grey30', fg='green
 
 Button(window, text="Конус", width=14, height=2, bg='grey30', fg='green yellow', command=draw_cone) \
     .grid(row=11, column=5, padx=5, pady=5, sticky='nsew')
-
+#
 Button(window, text="Exit", bg='grey20', fg='green yellow', command=exit) \
     .grid(row=12, column=0, columnspan=6, padx=5, pady=5, sticky='nsew')
 
--- shapes.py
+++ shapes.py
@@ -1,66 +1,25 @@
-from tkinter import *
+class Shape:
 
-window = Tk()
-window.title("Холст")
-canvas = Canvas(window, width=640, height=480, bg='grey20')
-canvas.pack()
+    _title = 'Фигура'
 
+    def area(self):
+        pass
 
-def draw_circle():
-    canvas.delete("all")
-    canvas.create_oval(220, 140, 420, 340, width=2, fill='grey20', outline='green yellow')
+    def perimetr(self):
+        pass
 
 
-def draw_square():
-    canvas.delete("all")
-    canvas.create_polygon(220, 140, 420, 140, 420, 340, 220, 340, width=2, fill='grey20', outline='green yellow')
+class Square(Shape):
 
+    _title = 'Квадрат'
 
-def draw_rectangle():
-    canvas.delete("all")
-    canvas.create_polygon(220, 200, 420, 200, 420, 280, 220, 280, width=2, fill='grey20', outline='green yellow')
+    def __init__(self, x):
+        super().__init__()
 
+        self.__x = x
 
-def draw_triangle():
-    canvas.delete("all")
-    canvas.create_polygon(320, 140, 220, 340, 420, 340, width=2, fill='grey20', outline='green yellow')
+    def area(self):
+        return self.__x ** 2
 
-
-def draw_trapeze():
-    canvas.delete("all")
-    canvas.create_polygon(270, 200, 370, 200, 420, 280, 220, 280, width=2, fill='grey20', outline='green yellow')
-
-
-def draw_rhombus():
-    canvas.delete("all")
-    canvas.create_polygon(320, 140, 370, 240, 320, 340, 270, 240, width=2, fill='grey20', outline='green yellow')
-
-
-def draw_sphere():
-    canvas.delete("all")
-    pass
-
-
-def draw_cube():
-    canvas.delete("all")
-    pass
-
-
-def draw_parallelepiped():
-    canvas.delete("all")
-    pass
-
-
-def draw_pyramid():
-    canvas.delete("all")
-    pass
-
-
-def draw_cylinder():
-    canvas.delete("all")
-    pass
-
-
-def draw_cone():
-    canvas.delete("all")
-    pass
+    def perimetr(self):
+        return self.__x * 4
